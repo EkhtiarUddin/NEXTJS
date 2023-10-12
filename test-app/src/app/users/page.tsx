21faf23e23b91d448b2bbfdff97054096cb54bcb
EkhtiarUddin <uddinikhtiar6@gmail.com>
2023-10-13 00:44:49 +0600

    data fetching and caching

--- test-app/src/app/users/page.tsx
+++ test-app/src/app/users/page.tsx
@@ -7,7 +7,8 @@ interface User {
 
 const UsersPage = async () => {
    
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const res = await fetch('https://jsonplaceholder.typicode.com/users',
+    {next: {revalidate: 10}}); //caching behavious is only implemented in fetch function
     const users: User[] = await res.json();
     return (
         <main>
