78cd7a4cbb7080a9f898df42a1eafe8dd5f5510b
EkhtiarUddin <uddinikhtiar6@gmail.com>
2023-10-13 14:31:46 +0600

    get method

--- curd-api/src/app/api/blogs/route.ts
+++ curd-api/src/app/api/blogs/route.ts
@@ -1,5 +1,16 @@
+import { NextResponse } from "next/server";
+import { getPosts } from "../../../../lib/data";
+
 export const GET = async (req: Request, res: Response) => {
-    console.log("GET REQUEST");
+    try {
+        const posts = getPosts();
+        return NextResponse.json({message: "OK", posts},{status: 200});
+    } catch (error){
+        return NextResponse.json(
+            { message: "Error", error},
+            {status: 500,}
+        );
+    }
 };
 
 export const POST = async (req: Request, res: Response) => {
